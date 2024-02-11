50513ea2d8898eb36fabf62a02fd9ba052f8f205
Jacob <admin@auroraweb.com.au>
2024-02-11 21:23:11 +1100

    Add UserMenu component to Navbar and update Search component

---
+++ app/components/List.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+interface ListProps {
+  getItems: () => number[]; // adjust the return type based on your actual data
+}
+
+const List: React.FC<ListProps> = ({ getItems }) => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    setItems(getItems());
+    console.log("updating items");
+  }, [getItems]);
+  return items.map((item, index) => <div key={index}>{item}</div>);
+};
+
+export default List;
---
+++ app/learn/useCallback/page.tsx
@@ -0,0 +1,7 @@
+import List from "@/app/components/List";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
