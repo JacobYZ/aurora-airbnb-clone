0fe86e4a3858feb7c89130c5895e7c689ee48b16
Jacob <admin@auroraweb.com.au>
2024-02-15 17:40:57 +1100

    Add guest, room, and bathroom counters to RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import { FieldValues, set, useForm } from "react-hook-form";
 import { title } from "process";
 import CountrySelect, { CountrySelectValue } from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 enum STEPS {
   CATEGORY = 0,
   LOCATION = 1,
@@ -42,6 +43,9 @@ const RentModal = () => {
   });
   const category = watch("category");
   const location = watch("location");
+  const guestCount = watch("guestCount");
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
   const Map = useMemo(
     () => dynamic(() => import("../Map"), { ssr: false }),
     [location]
@@ -105,6 +109,36 @@ const RentModal = () => {
           onChange={(value) => setCustomeValue("location", value)}
         />
         <Map center={location?.latlng} />
+      </div>
+    );
+  }
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some details about your place"
+          subtitle="Set the scene for guests"
+        />
+        <Counter
+          title="Guests"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomeValue("guestCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => setCustomeValue("roomCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have??"
+          value={bathroomCount}
+          onChange={(value) => setCustomeValue("bathroomCount", value)}
+        />
       </div>
     );
   }
