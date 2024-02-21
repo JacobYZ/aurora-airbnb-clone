df7ef1df3e5538cae45ee79b3a66d32bc61766ed
Jacob <admin@auroraweb.com.au>
2024-02-21 16:22:14 +1100

    Add currentUser prop to FavoritesClient component and update title in TripsClient component

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -21,6 +21,7 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {listings.map((listing) => (
           <ListingCard
+            currentUser={currentUser}
             key={listing.id}
             data={listing}
           />
